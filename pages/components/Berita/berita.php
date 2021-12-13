fc07726049d1a4cfefc82a9a326a03ee5d22947c
rizkipajar <rizkipajar47@gmail.com>
2021-12-13 13:26:49 +0000

    tambah berita di card 2

--- pages/components/Berita/berita.php
+++ pages/components/Berita/berita.php
@@ -24,12 +24,12 @@
       </div>
       <div class="card">
         <div class="card-top">
-          <img src="img2.jpg" alt="Blog Name">
+          <img src="/src/content/karimunjawa.jpg" alt="karimunjawa">
         </div>
         <div class="card-info">
-          <h2>How to Build HTML Minifier</h2>
+          <h2>Pulau Karimun Jawa : Eksotisme Surga Tersembunyi Di Selat Jawa</h2>
           <span class="date">Monday, Jan 20, 2021</span>
-          <p class="excerpt">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s</p>
+          <p class="excerpt">Karena bentuknya yang berupa kepulauan, tentu saja Karimun  Jawa memberikan keindahan pemandangan laut dan pulau yang membentuk keindahan baik ketika matahari terbit atau matahari terbenam.</p>
         </div>
         <div class="card-bottom flex-row">
           <a href="#" class="read-more">Read Full Blog</a>
