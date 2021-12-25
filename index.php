9aaa6af3bbad9af324fa7df13e592fa31772f66d
muhammadlutvzaini <muhammadlutvzaini@gmail.com>
2021-12-25 22:33:31 +0700

    add background color

--- index.php
+++ index.php
@@ -4,6 +4,7 @@
   <meta charset="UTF-8" />
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <meta name="theme-color" content="#2cb67d"/>
   <link rel="stylesheet" type="text/css" href="styles/global.css">
   <title>JeparaUMKM</title>
   <link rel="shortcut icon" href="src/layout/favicon.ico" type="image/x-icon">
