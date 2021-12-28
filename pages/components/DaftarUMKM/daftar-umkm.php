b5091cb882f755b78be2a98e22257c9e4cb83937
rizkipajar <rizkipajar47@gmail.com>
2021-12-28 08:38:40 +0000

    isi Daftar UMKM sebanyak 20

--- pages/components/DaftarUMKM/daftar-umkm.php
+++ pages/components/DaftarUMKM/daftar-umkm.php
@@ -23,118 +23,118 @@
                 <td data-label="Kategori">Perdagangan dan Reparasi</td>
             </tr>
             <tr>
-                <td data-label="NIM">2</td>
-                <td data-label="Nama">CITRA KREASI</td>
-                <td data-label="Jurusan">Kecil</td>
-                <td data-label="Angkatan">Perdagangan dan Reparasi</td>
+                <td data-label="No">2</td>
+                <td data-label="Nama Usaha">CITRA KREASI</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Perdagangan dan Reparasi</td>
             </tr>
             <tr>
-                <td data-label="NIM">16.11.0666</td>
-                <td data-label="Nama">Andi Stya Anggara</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2016</td>
+                <td data-label="No">3</td>
+                <td data-label="Nama Usaha">SASMINAH / DATUK JAYA SABLON</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Perdagangan, Hotel, Resto</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">4</td>
+                <td data-label="Nama Usaha">KUB MAJU BERKAH</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Perdagangan dan Reparasi</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+            <td data-label="No">5</td>
+                <td data-label="Nama Usaha">UKM CHANDRA JAYA</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Industri Pengolahan</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+            <td data-label="No">6</td>
+                <td data-label="Nama Usaha">TAIN FURNITURE</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Furniture</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">7</td>
+                <td data-label="Nama Usaha">CV. Rotan Jepara</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Furniture dan Handycraft</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">8</td>
+                <td data-label="Nama Usaha">Rajungan Berkah Laut</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Makanan/Minuman</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">9</td>
+                <td data-label="Nama Usaha">TENUN MITRA WARNA</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Fashion</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">10</td>
+                <td data-label="Nama Usaha">BATIK DEWI KUNTI</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Fashion</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">11</td>
+                <td data-label="Nama Usaha">Fam's Snack</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Industri Pengolahan</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">12</td>
+                <td data-label="Nama Usaha">CV. Serasi Indonesia Jaya</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Perdagangan, Hotel, Resto</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">13</td>
+                <td data-label="Nama Usaha">FARA COLETTION</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Industri Pengolahan</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">14</td>
+                <td data-label="Nama Usaha">UKM FAZA</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Industri Pengolahan</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">15</td>
+                <td data-label="Nama Usaha">UKM MASIYATI</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Industri Pengolahan</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">16</td>
+                <td data-label="Nama Usaha">UKM SYAFANA</td>
+                <td data-label="Klasifikasi">Mikro</td>
+                <td data-label="Kategori">Industri Pengolahan</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">17</td>
+                <td data-label="Nama Usaha">UKM KASMANTO</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Pengrajin Ukir Kayu</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">18</td>
+                <td data-label="Nama Usaha">Gendhis Batik</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Handycraft</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">19</td>
+                <td data-label="Nama Usaha">Dapur Jihan</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Makanan/Minuman</td>
             </tr>
             <tr>
-                <td data-label="NIM">15.11.9023</td>
-                <td data-label="Nama">Adi Pracoyo</td>
-                <td data-label="Jurusan">Informatika</td>
-                <td data-label="Angkatan">2015</td>
+                <td data-label="No">20</td>
+                <td data-label="Nama Usaha">Pentol Pa'e Jepara</td>
+                <td data-label="Klasifikasi">Kecil</td>
+                <td data-label="Kategori">Makanan/Minuman</td>
             </tr>
         </tbody>
     </table>
